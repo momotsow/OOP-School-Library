1cc211e14efb60037dbac6545dc6eba82b69c196
momotsow <kgomotsomkansi@gmail.com>
2023-02-06 21:35:48 +0200

    Create teacher refactor

--- app.rb
+++ app.rb
@@ -55,19 +55,6 @@ class App
     option_checker(answer)
   end
 
-  def create_teacher
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Specialization: '
-    specialization = gets.chomp
-    print 'Name: '
-    name = gets.chomp
-
-    teacher = Teacher.new(age, specialization, name)
-    @people.push(teacher)
-    puts 'Teacher is created successfully'
-  end
-
   def create_book
     print 'Title: '
     title = gets.chomp
